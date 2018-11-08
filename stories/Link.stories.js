93454360bac0bf55e1e083a3c85dc4fe2715cc1c
sutter <laurent@sutterlity.fr>
2018-11-08 11:01:07 +0100

    💥 FIX - component name

--- stories/Link.stories.js
+++ stories/Link.stories.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text, boolean } from '@storybook/addon-knobs';
-import AppLink from '../src/components/atoms/Link';
+import Link from '../src/components/atoms/Link';
 
 storiesOf('Atom|Link', module).add('base', () => (
-  <AppLink
+  <Link
     to={text('to', 'http://sutterlity.fr')}
     disabled={boolean('disabled', false)}
   >
     {text('children', 'Laurent Sutterlity')}
-  </AppLink>
+  </Link>
 ));
