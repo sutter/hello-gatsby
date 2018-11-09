a6ed2ddab4301579aa8a27d080876f34d422ce96
sutter <laurent@sutterlity.fr>
2018-11-09 20:23:54 +0100

    ✨ FEATURE - Add new colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,5 +1,8 @@
 export const light = '#ffffff';
-export const neutral = '#212121';
+export const neutralDark = '#212121';
+export const neutral = '#666';
+export const neutralLight = '#888';
+export const neutralLighter = '#ccc';
 
 export const clr1 = '#663399';
 export const clr1Light = '#8C65B3';
