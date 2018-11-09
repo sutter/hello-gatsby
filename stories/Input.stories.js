41e7b6c28c998d5bd87463d30f0f8149bf20be3c
sutter <laurent@sutterlity.fr>
2018-11-09 22:50:48 +0100

    ✨ FEATURE - add InputSelect atom

--- stories/Input.stories.js
+++ stories/Input.stories.js
@@ -5,6 +5,7 @@ import { text, boolean, select } from '@storybook/addon-knobs';
 import InputLabel from '../src/components/atoms/input/InputLabel';
 import InputBase, { types } from '../src/components/atoms/input/InputBase';
 import InputTextarea from '../src/components/atoms/input/InputTextarea';
+import InputSelect from '../src/components/atoms/input/InputSelect';
 
 storiesOf('Atom|Input/InputLabel', module).add('base', () => (
   <InputLabel>{text('children', 'Label')}</InputLabel>
@@ -28,4 +29,14 @@ storiesOf('Atom|Input/InputTextarea', module).add('base', () => (
     readOnly={boolean('readOnly', false)}
     onChange={action('onChange')}
   />
+));
+
+storiesOf('Atom|Input/InputSelect', module).add('base', () => (
+  <InputSelect>
+    <option value="value1">Value 1</option>
+    <option value="value2" defaultValue>
+      Value 2
+    </option>
+    <option value="value3">Value 3</option>
+  </InputSelect>
 ));
