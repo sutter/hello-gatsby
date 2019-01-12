d5336c9cf51595529dd2e6684b666714c7948bab
sutter <laurent@sutterlity.fr>
2019-01-12 14:33:09 +0100

    🎨 POLISH - In button add theme and size props

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,32 +1,69 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 import { rem } from 'polished';
-import * as radius from '../../styles/radius.js';
+import * as radius from '../../styles/radius';
 import * as color from '../../styles/colors';
 import * as state from '../../styles/state';
 
+const linkState = '&:hover, &:focus, &:active';
+
+export const themes = {
+  primary: css`
+    color: ${color.light};
+    background: ${color.clr1};
+    border-color: ${color.clr1};
+    ${linkState} {
+      color: ${color.light};
+      background: ${color.clr1Light};
+      border-color: ${color.clr1Light};
+    }
+  `,
+  neutral: css`
+    color: ${color.clr1};
+    background: 'transparent';
+    border-color: ${color.clr1Light};
+    ${linkState} {
+      color: ${color.clr1};
+      background: 'transparent';
+      border-color: ${color.clr1};
+    }
+  `,
+};
+
+export const sizes = {
+  M: css`
+    min-height: ${rem(40)};
+    padding: ${rem(12)} ${rem(20)};
+    font-size: inherit;
+  `,
+  S: css`
+    min-height: ${rem(30)};
+    padding: ${rem(8)} ${rem(10)};
+    font-size: ${rem(12)};
+  `,
+};
+
 const Button = styled.a`
   display: inline-flex;
-  min-height: ${rem(40)};
-  padding: ${rem(12)} ${rem(20)};
   text-align: center;
   text-decoration: none;
-  color: ${color.light};
-  background: ${color.clr1};
   font-family: inherit;
-  font-size: inherit;
   line-height: 1;
-  border: none;
   border-radius: ${radius.S};
+  border-style: solid;
+  border-width: 1px;
   opacity: ${props => (props.disabled ? state.disabledOpacity : 1)};
   pointer-events: ${props => (props.disabled ? 'none' : 'auto')};
   cursor: pointer;
   user-select: none;
-  &:hover,
-  &:focus,
-  &:active {
-    background-color: ${color.clr1Light};
-    color: ${color.light};
-  }
+  transition: all 0.2s ease-in-out;
+  ${props => themes[props.theme]};
+  ${props => sizes[props.size]};
 `;
+
+Button.defaultProps = {
+  theme: 'primary',
+  size: 'M',
+};
 
 export default Button;
--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text, boolean, select } from '@storybook/addon-knobs';
-import Button from '../src/components/atoms/Button';
+import Button, { sizes, themes } from '../src/components/atoms/Button';
 
 storiesOf('Atom|Button', module).add('base', () => (
   <Button
     as={select('as', ['a', 'button', 'div'], 'button')}
     href={text('href', 'http://sutterlity.fr')}
     disabled={boolean('disabled', false)}
+    theme={select('theme', Object.keys(themes), Object.keys(themes)[0])}
+    size={select('size', Object.keys(sizes), Object.keys(sizes)[0])}
   >
     {text('children', 'My button')}
   </Button>
