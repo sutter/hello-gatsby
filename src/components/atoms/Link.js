9da6bc1f9b64c61e32787764ebf98739cfad5529
sutter <laurent@sutterlity.fr>
2019-01-12 15:06:36 +0100

    🔨 REFACTORING - Link for automatique internal or external switch

--- src/components/atoms/Link.js
+++ src/components/atoms/Link.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import styled from '@emotion/styled';
-import * as link from '../../styles/links';
+import { Link as GatsbyLink } from 'gatsby';
 
-const Link = styled.a`
-  ${link.styleBase};
-`;
+const Element = styled.a``;
+
+const Link = ({ children, href, ...rest }) => {
+  return (
+    <Element {...rest} href={href} as={href ? 'a' : GatsbyLink}>
+      {children}
+    </Element>
+  );
+};
 
 export default Link;
