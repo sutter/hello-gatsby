98df046ed429498805950d155b92e16139c4f003
sutter <laurent@sutterlity.fr>
2019-01-13 13:23:32 +0100

    🔨 REFACTORING - Add constant for app

--- src/components/organims/AppHeader.js
+++ src/components/organims/AppHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { route } from '../../constants/app';
 import Link from '../atoms/Link';
 
 const Container = styled.header`
@@ -8,7 +9,7 @@ const Container = styled.header`
 
 const AppHeader = props => (
   <Container {...props}>
-    <Link to="/">Header</Link>
+    <Link to={route.home}>Header</Link>
   </Container>
 );
 
