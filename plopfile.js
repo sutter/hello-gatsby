ff96c8ac58ff1c9383c52de7fb48e15afb145b81
sutter <laurent@sutterlity.fr>
2019-06-29 15:56:06 +0200

    ✨ FEATURE - add plop generator

--- plopfile.js
+++ plopfile.js
@@ -1,6 +1,6 @@
-// Templates
 const path = require('path');
 
+// Templates
 const componentTemplate = `
 import * as React from 'react';
 import { css } from '@emotion/core';
@@ -20,11 +20,72 @@ const {{componentName}}: React.FunctionComponent<{{componentName}}Props> = ({ ch
 export default {{componentName}};
 `;
 
+const pageTemplate = `
+import * as React from 'react';
+import Wrapper from '../components/base/Wrapper';
+import App from '../components/layout/App';
+
+const {{pageName}}Page = () => {
+  return (
+    <App>
+      <Wrapper>
+        {{pageName}} page
+      </Wrapper>
+    </App>
+  );
+};
+
+export default {{pageName}}Page;
+`;
+
+/*
+  Setup "plop" as a cli generator
+  see https://github.com/amwmedia/plop
+*/
+
 module.exports = function(plop) {
   plop.addPrompt('directory', require('inquirer-directory'));
+
   // Add helpers
   plop.addHelper('componentsPath', function(p) {
     return path.resolve(plop.getPlopfilePath() + '/src/components', p);
+  });
+
+  plop.addHelper('snakecase', function(name) {
+    return name
+      .split(/(?=[A-Z])/)
+      .map(s => s.toLowerCase())
+      .join('-');
+  });
+
+  // Page generator
+  plop.setGenerator('page', {
+    description: 'create a new gatsby page',
+    prompts: [
+      {
+        type: 'input',
+        name: 'pageName',
+        message: 'Pick a name for the page (UpperCamelCase)',
+        validate: function(value) {
+          if (/.+/.test(value)) {
+            if (/([A-Z][a-z0-9]+)+/.test(value)) {
+              return true;
+            } else {
+              return 'format is invalid';
+            }
+          } else {
+            return 'file name is required';
+          }
+        },
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/pages/{{snakecase pageName}}.tsx',
+        template: pageTemplate,
+      },
+    ],
   });
 
   // Component generator
