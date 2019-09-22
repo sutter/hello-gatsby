f5b44f6c7c55eaf1f031fd7562024a31d55e3b9f
sutter <laurent@sutterlity.fr>
2019-09-22 12:07:03 +0200

    Remove config/assets gatsby-source-filesystem options

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,13 +63,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: 'assets',
-      },
-    },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
